c6bf1c43e5ced19a92e42b64378ca1a0433d20c8
MickeyFinnn <bilousovman@gmail.com>
2020-05-11 13:45:31 +0000

    formated JS

--- js/index.js
+++ js/index.js
@@ -1,40 +1,39 @@
-const btnDigits = document.querySelectorAll('.digits button');
-btnDigits.forEach(digit => digit.addEventListener('click', digitPressed));
+const btnDigits = document.querySelectorAll(".digits button");
+btnDigits.forEach((digit) => digit.addEventListener("click", digitPressed));
 
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
 function digitPressed(ev) {
-    display.value += ev.target.innerText;
+  display.value += ev.target.innerText;
 }
 
-const btnOpers = document.querySelectorAll('.opers button');
-btnOpers.forEach( digit => digit.addEventListener('click', operPressed));
+const btnOpers = document.querySelectorAll(".opers button");
+btnOpers.forEach((digit) => digit.addEventListener("click", operPressed));
 
 function operPressed(ev) {
-   display.value += ev.target.innerText;
+  display.value += ev.target.innerText;
 }
 
-
-const btnEq = document.querySelector('.eq');
-btnEq.addEventListener('click', eqPressed);
+const btnEq = document.querySelector(".eq");
+btnEq.addEventListener("click", eqPressed);
 
 function eqPressed() {
-    display.value = eval(display.value);
+  display.value = eval(display.value);
 }
 
-const btnClear = document.querySelector('.clear');
-btnClear.addEventListener('click', clearPressed);
+const btnClear = document.querySelector(".clear");
+btnClear.addEventListener("click", clearPressed);
 
 function clearPressed() {
-    display.value = "";
+  display.value = "";
 }
 
-const btnBack = document.querySelector('.back');
-btnBack.addEventListener('click', backPressed);
+const btnBack = document.querySelector(".back");
+btnBack.addEventListener("click", backPressed);
 
 function backPressed() {
-    let exp = display.value;
-    display.value = exp.substring(0,exp.length-1);
+  let exp = display.value;
+  display.value = exp.substring(0, exp.length - 1);
 }
 
 const btnZero = document.querySelector(".zero");
